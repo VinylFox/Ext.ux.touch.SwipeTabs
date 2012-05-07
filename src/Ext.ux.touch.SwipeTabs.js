07bb74e356631f27bc088e6a02273f46cdffd9e2
Jean Plouin <jean.plouin@appitforme.com>
2012-05-07 09:00:34 +0200

    updated to st2.0.1 'ish code

--- src/Ext.ux.touch.SwipeTabs.js
+++ src/Ext.ux.touch.SwipeTabs.js
@@ -1,4 +1,4 @@
-Ext.ns('Ext.ux.touch');
+//Ext.ns('Ext.ux.touch');
 /**
  * @author Shea Frederick - http://www.vinylfox.com
  * @class Ext.ux.touch.SwipeTabs
@@ -13,89 +13,93 @@ Ext.ns('Ext.ux.touch');
  }
  * </code></pre>
  */
-Ext.ux.touch.SwipeTabs = Ext.extend(Ext.util.Observable, {
-  // private
-  init: function(cmp){
-    this.cmp = cmp;
-    this.swipeRight = [];
-    this.swipeLeft = []; 
-    cmp.on('painted', this.initSwipeHandlers, this);
-  },
-  // private
-  initSwipeHandlers: function(){
-    this.cmp.til = this.cmp.items.length-1;
-    this.cmp.items.each(function(itm, i){
-      itm.idx = i;
-      if (itm.allowSwipe !== false && !itm.docked){
-        if (itm.getLayout().type === 'card'){
-          if (itm.rendered){
-            this.initChildSwipeHandlers(itm);
+Ext.define('Ext.ux.touch.SwipeTabs', {
+    mixins: ['Ext.mixin.Observable'],
+
+    alias: 'plugin.swipetabs',
+    // private
+    init: function(cmp){
+      this.cmp = cmp;
+      this.swipeRight = [];
+      this.swipeLeft = []; 
+      cmp.on('painted', this.initSwipeHandlers, this);
+    },
+    // private
+    initSwipeHandlers: function(){
+      this.cmp.til = this.cmp.items.length-1;
+      this.cmp.items.each(function(itm, i){
+        itm.idx = i;
+        if (itm.allowSwipe !== false && !itm.docked){
+          if (itm.getLayout().type === 'card'){
+            if (itm.rendered){
+              this.initChildSwipeHandlers(itm);
+            }else{
+              itm.on('painted', this.initChildSwipeHandlers, this);
+            }
           }else{
-            itm.on('painted', this.initChildSwipeHandlers, this);
-          }
-        }else{
-          if (itm.rendered){
-            this.addSwipe(itm, i);
-          }else{
-            itm.on('painted', function(){ 
-             this.addSwipe(itm, i);
-            }, this);
+            if (itm.rendered){
+              this.addSwipe(itm, i);
+            }else{
+              itm.on('painted', function(){ 
+               this.addSwipe(itm, i);
+              }, this);
+            }
           }
         }
-      }
-    },this)
-  },
-  // private
-  initChildSwipeHandlers: function(itm){
-    var i = itm.idx;
-    itm.items.each(function(itm){
-      if (itm.rendered){
-        this.addSwipe(itm, i);
-      }else{
-        itm.on('painted', function(){
+      },this)
+    },
+    // private
+    initChildSwipeHandlers: function(itm){
+      var i = itm.idx;
+      itm.items.each(function(itm){
+        if (itm.rendered){
           this.addSwipe(itm, i);
-        }, this);
+        }else{
+          itm.on('painted', function(){
+            this.addSwipe(itm, i);
+          }, this);
+        }
+      }, this);
+    },
+    // private
+    addSwipe: function(itm, i){
+      if (i === 0){
+        this.addSwipeLeft(itm, i);
+      }else if(i === this.cmp.til){
+        this.addSwipeRight(itm, i);
+      }else{
+        this.addSwipeLeft(itm, i);
+        this.addSwipeRight(itm, i);
       }
-    }, this);
-  },
-  // private
-  addSwipe: function(itm, i){
-    if (i === 0){
-      this.addSwipeLeft(itm, i);
-    }else if(i === this.cmp.til){
-      this.addSwipeRight(itm, i);
-    }else{
-      this.addSwipeLeft(itm, i);
-      this.addSwipeRight(itm, i);
+    },
+    // private
+    addSwipeLeft: function(itm, i){
+      this.swipeLeft.push(itm);
+      itm.element.on('swipe', this.onSwipeLeft, this);
+    },
+    // private
+    onSwipeLeft: function(ev) {
+      if(ev.direction == "left") {
+        this.cmp.setActiveItem(this.cmp.getActiveItem().idx + 1);
+      }
+    },
+    // private
+    addSwipeRight: function(itm, i){
+      this.swipeRight.push(itm);
+      itm.element.on('swipe', this.onSwipeRight, this);
+    },
+    // private
+    onSwipeRight: function(ev) {
+      if(ev.direction == "right") {
+        this.cmp.animateActiveItem(this.cmp.getActiveItem().idx - 2, {
+          type : 'slide',
+          direction : 'right'
+        });
+      }
+    },
+    // private
+    parentDestroy: function(){
+      
     }
-  },
-  // private
-  addSwipeLeft: function(itm, i){
-    this.swipeLeft.push(itm);
-    itm.element.on('swipe', this.onSwipeLeft, this);
-  },
-  // private
-  onSwipeLeft: function(ev) {
-    if(ev.direction == "left") {
-      this.cmp.setActiveItem(this.cmp.getActiveItem().idx + 1);
-    }
-  },
-  // private
-  addSwipeRight: function(itm, i){
-    this.swipeRight.push(itm);
-    itm.element.on('swipe', this.onSwipeRight, this);
-  },
-  // private
-  onSwipeRight: function(ev) {
-    if(ev.direction == "right") {
-      this.cmp.animateActiveItem(this.cmp.getActiveItem().idx - 2, {
-        type : 'slide',
-        direction : 'right'
-      });
-    }
-  },
-  // private
-  parentDestroy: function(){
-    
-  }
+
 });
